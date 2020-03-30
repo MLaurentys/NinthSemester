aaa9f9b42d16a3fc0cd780daa7e064514c1ef610
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-03-30 12:00:10 -0300

    start

---
+++ MAC0331/pe01/line_intersection.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Scanline algorithm for line intersections"""
+
+from geocomp.common.segment import Segment
+from geocomp.common import control
+from geocomp.common.guiprim import *
+import math
+
+def Scanline (segments):
