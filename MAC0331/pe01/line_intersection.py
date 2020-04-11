9b31a2b96f1eb282178a58a589028342ca754d09
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-04-11 14:09:35 -0300

    copied finished version over

--- MAC0331/pe01/line_intersection.py
+++ MAC0331/pe01/line_intersection.py
@@ -54,8 +54,9 @@ class Node_Seg:
         super().__init__()
         self.left = None
         self.right = None
-        self.start = Node(seg[0])
-        self.end = Node(seg[1])
+        self.seg = seg
+        self.start = Node(seg.init)
+        self.end = Node(seg.to)
 
     def get_val(self):
         return self.start, self.end
@@ -65,8 +66,14 @@ class Node_Seg:
         self.end = end
 
     def __lt__(self, other):
-        return self.start < other.start
-
+        if self.start.y < other.start.y:
+            return True
+        else:
+            if (other.start.y < self.start.y):
+                return False
+            else:
+                return self.end.y < other.end.y
+                
 # BST data structure that should work for any comparable (class that implements __lt__)
 class ABB:
     def __init__(self, Nd_constr):
@@ -131,6 +138,36 @@ class ABB:
     def is_empty(self):
         return self._root == None
 
+    
+
+    def _get_neighbour(self, node):
+        def find_left(start, node):
+                ret = None
+                if(start != None):
+                    if (start < node):
+                        temp = find_left(start.right, node)
+                        ret = temp if (temp) else start
+                    else:
+                        ret = find_left(start.left, node)
+                return ret
+        def find_right(start, node):
+            ret = None
+            if(start != None):
+                if (node < start):
+                    temp = find_right(start.left, node)
+                    ret = temp if(temp) else start
+                else:
+                    ret = find_right(start.right, node)
+            return ret
+        
+        return find_left(self._root, node), find_right(self._root, node)
+
+    def get_neighbours(self, val):
+        nd = self._Node(val)
+        ns = self._get_neighbour(nd)
+        ns1 = ns[0].seg if(ns[0]) else None
+        ns2 = ns[1].seg if(ns[1]) else None
+        return ns1, ns2
 ##############################
 ###
 ### PE 1 - START
@@ -216,10 +253,27 @@ def seg_intersects_properly (seg1, seg2):
     return (at_left(a,c,d) != at_left(b,c,d)) and \
             (at_left(c,a,b) != at_left(d,a,b))
 
+# Get intersection point
+def intersection (seg1, seg2):
+    def line(p1, p2):
+        A = (p1.y - p2.y)
+        B = (p2.x - p1.x)
+        C = (p1.x*p2.y - p2.x*p1.y)
+        return A, B, -C
+    l1 = line(seg1.init, seg1.to)
+    l2 = line(seg2.init, seg2.to)
+    d = l1[0]*l2[1] - l1[1]*l2[0]
+    dx = l1[2]*l2[1] - l1[1]*l2[2]
+    dy = l1[0] * l2[2] - l1[2] * l2[0]
+    x = dx/d
+    y = dy/d
+    return x,y
+
 # Whether segments cross
 def seg_intersects(seg1, seg2):
     a,b,c,d = seg1.init, seg1.to, seg2.init, seg2.to
-    if (seg_intersects_properly):
+    return prim.intersect(a,b,c,d)
+    if (seg_intersects_properly(seg1, seg2)):
         return True
     return in_between(a,c,d) or in_between(b,c,d) or \
             in_between(c,a,b) or in_between(d,a,b)
@@ -229,41 +283,78 @@ def seg_intersects(seg1, seg2):
 ## Scanline
 ##
 
+def verify_n_intersection (seg, neigh, msg):
+    if (not neigh): return False
+    seg.hilight(color_line="blue")
+    neigh.hilight(color_line="blue")
+    if (seg_intersects(seg, neigh)):
+        int_point = intersection(seg,neigh)
+        control.plot_disc(int_point[0], int_point[1], "yellow", 6.0)
+        print("Enquanto comparava", seg, neigh)
+        print(msg, int_point[0], int_point[1])
+        return True
+    control.sleep()
+    seg.hilight(color_line="green")
+    neigh.hilight(color_line="green")
+
+def treat_left(s, bst):
+    msg = "ACHOU INSERINDO:"
+    s.hilight(color_line="green")
+    control.sleep()
+    bst.insert(s)
+    ns = bst.get_neighbours(s)
+    ret = verify_n_intersection(s, ns[0], msg)
+    if (ret) : return True
+    ret = verify_n_intersection(s, ns[1], msg)
+    if (ret) : return True
+
+def treat_right (s, bst):
+    msg = "ACHOU REMOVENDO:"
+    ret = False
+    s.hilight(color_line="cyan")
+    control.sleep()
+    bst.remove(s)
+    ns = bst.get_neighbours(s)
+    if(ns[0] and ns[1]):
+        ret = verify_n_intersection(ns[0], ns[1], msg)
+    return ret
+    control.sleep()
+
 # Scanline algorithm to find all intersections between segments in a plane
+# Fixing typos
+# highlight = segment.hilight
 def Scanline (segments):
-    print(type(segment))
     fix_segments(segments)
-    print(segments)
     # should NEVER change the order of segments after sorted
     segments = sorted(segments, key=functools.cmp_to_key(compare_segments))
     heap, hmap = make_event_points(segments)
-
-    # for i in range (len(segments)):
-    #     print(str(i) +": " + str(segments[i]))
-    #     segments[i].plot()
+    bst = ABB(Node_Seg)
+    for i in range (len(segments)):
+        #print(str(i) +": " + str(segments[i]))
+        segments[i].plot()
     
     while (not heap.empty()):
         pt = heap.get()
-        print_aux1(pt, segments)
+        circ = control.plot_circle(pt.node.x, pt.node.y, "green", 2)
+        #print_aux1(pt, segments)
+        control.sleep()
+        if (len(pt.left) + len(pt.right) > 1):
+            control.plot_disc(pt.node.x, pt.node.y, "yellow", 6.0)
+            print("ACHOU IMEDIATAMENTE: no ponto ", pt.node.x, pt.node.y )
+            return True
         # Start of a new segment
         for seg in pt.left:
-            circ = control.plot_circle(pt.node.x, pt.node.y, "green", 2)
-            segments[seg].highlight
-            control.sleep()
-
-            control.plot_delete(circ)
+            if(treat_left(segments[seg], bst)):
+                return True
         #End of an exhisting segment
         for seg in pt.right:
-            circ = control.plot_circle(pt.node.x, pt.node.y, "yellow", 2)
-            control.sleep()
+            if(treat_right(segments[seg], bst)):
+                return True
 
-            control.plot_delete(circ)
         #Intersections on the point (no specific order)
         for segs in pt.intersections:
-            circ = control.plot_circle(pt.node.x, pt.node.y, "red", 2)
             control.sleep()
-
-            control.plot_delete(circ)
+        control.plot_delete(circ)
 
 # auxiliary prints
 def print_aux1(pt, segs):
