6f77451d30c2786554731e03d52fbd9e3df889d2
MLaurentys <matheus_t.laurentys@hotmail.com>
2020-06-28 19:11:09 -0300

    fixed data generation format to match the expected in html

--- MAC0350/proj4/django_proj/polls/views.py
+++ MAC0350/proj4/django_proj/polls/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db import connection
+from collections import namedtuple
+from django.template import loader
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+def query1(request):
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT * FROM usuario')
+        result = dictfetchall(cursor)
+        print(result)
+    
+    template = loader.get_template('queries/query1.html')
+    context = {'query1_result_list': result,}
+    
+    return HttpResponse(template.render(context, request))
 
 
 def index(request):
